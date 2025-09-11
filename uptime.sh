0e3532dbca203d55e0408f23d0c863a8dcdb219d
anonymmized <lih0deev@inbox.ru>
2025-09-11 21:39:39 +0300

    Infuted config in Crontab

--- uptime.sh
+++ uptime.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+SCRIPT_PATH="$(pwd)/uptime.sh"
+MARKER="# UPTIME_CHECK"
+CRONTAB_CONTENT=$(crontab -l 2>/dev/null || true)
+NEW_CRONTAB=$(
+  { echo "$CRONTAB_CONTENT" | grep -v "$MARKER"
+    echo "* * * * * $SCRIPT_PATH >> /tmp/uptime.log 2>&1 $MARKER"; }
+)
+echo "$NEW_CRONTAB" | crontab -
+echo "[*] Crontab configuration added"
 uptime_line=$(w | head -n1)
-
 uptime_part=$(echo "$uptime_line" | sed -E 's/.*up (.*), [0-9]+ user.*/\1/')
 
 case "$uptime_part" in
