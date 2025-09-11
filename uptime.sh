e8ed37ca3512258c4d55b39bd6ebe16ca475f46c
anonymmized <lih0deev@inbox.ru>
2025-09-11 13:05:45 +0300

    Checking activity

--- uptime.sh
+++ uptime.sh
@@ -3,4 +3,14 @@
 number=$(w | head -n1 | awk '{print $3;}' | tr -d ',')
 duration=$(w | head -n1 | awk '{print $4;}' | tr -d ',')
 
-echo "$number:$duration"
+arr=("secs" "min" "days")
+
+if [[ " ${arr[*]} " == *" $duration "* ]]; then
+    echo "Активен либо более суток, либо менее часа"
+else
+    IFS=":" read -r hours minutes <<< "$number"
+    echo "Активен $hours часов $minutes минут"
+fi
+
+
+#echo "$number:$duration"
