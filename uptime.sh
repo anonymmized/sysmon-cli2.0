db6e072003aab79fa1025ccc1f008c8a8f0a6853
anonymmized <lih0deev@inbox.ru>
2025-09-13 19:40:16 +0300

    Add get_os_information function

--- uptime.sh
+++ uptime.sh
@@ -3,6 +3,17 @@
 
 set -euo pipefail
 
+get_os_information() {
+    kernel_type=$(uname -m)
+    kernel_ver=$(uname -r)
+    os_type=$(uname -s)
+    if [[ $os_type == "Darwin" ]]; then
+        echo -e "Kernel - $kernel_type\nOS - MacOS\nKernel version - $kernel_ver"
+    elif [[ $os_type == "Linux" ]]; then
+        echo -e "Kernel - $kernel_type\nOS - Linux\nKernel version - $kernel_ver"
+    fi
+}
+
 get_uptime_seconds_linux_host() {
   cut -d. -f1 /proc/uptime
 }
@@ -28,3 +39,4 @@ get_uptime_seconds() {
 }
 
 get_uptime_seconds
+get_os_information
