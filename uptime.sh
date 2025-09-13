bff5ef8daf3dcbb54eaa51e798c0e19f2947a5ee
anonymmized <lih0deev@inbox.ru>
2025-09-13 23:14:28 +0300

    Add information about users

--- uptime.sh
+++ uptime.sh
@@ -8,7 +8,7 @@ OS=$(uname -s)
 get_user_information() {
     cur_hostname=$(hostname)
     cur_username=$(whoami)
-
+    echo -e "Hostname: $cur_hostname\nUser: $cur_username"
 }
 
 get_cpu_information() {
@@ -27,7 +27,7 @@ get_cpu_information() {
 get_time_information() {
     cur_time=$(w | head -n1 | awk '{print $1}')
     cur_zone=$(ls -l /etc/localtime | awk -F/ '{print $(NF-1) "/" $NF}')
-    echo -e "Current time - $cur_time\nCurrent zone - $cur_zone"
+    echo -e "Current time: $cur_time\nCurrent zone: $cur_zone"
 }
 
 get_os_information() {
@@ -35,9 +35,9 @@ get_os_information() {
     kernel_ver=$(uname -r)
     os_type=$(uname -s)
     if [[ $os_type == "Darwin" ]]; then
-        echo -e "Kernel - $kernel_type\nOS - MacOS\nKernel version - $kernel_ver"
+        echo -e "Kernel: $kernel_type\nOS: MacOS\nKernel version: $kernel_ver"
     elif [[ $os_type == "Linux" ]]; then
-        echo -e "Kernel - $kernel_type\nOS - Linux\nKernel version - $kernel_ver"
+        echo -e "Kernel: $kernel_type\nOS: Linux\nKernel version: $kernel_ver"
     fi
 }
 
@@ -52,11 +52,7 @@ get_uptime_seconds() {
       local boot now
       boot="$(/usr/sbin/sysctl -n kern.boottime | /usr/bin/awk -F'[ ,]' '{print $4}')"
       now="$(/bin/date +%s)"
-      uptime_s=$(( now - boot))
-      days=$(( uptime_s / 86400 ))
-      hours=$(( (uptime_s / 86400) / 3600 ))
-      minutes=$(( (uptime_s % 3600) / 60 ))
-      echo "Active $days дней, $hours часов, $minutes минут"
+      echo $(( now - boot ))
       ;;
     *)
       echo "Unsupported OS: $OSTYPE" >&2
@@ -67,15 +63,11 @@ get_uptime_seconds() {
 
 uptime_s=$(get_uptime_seconds)
 
-printf -v uptime_hms "%02d:%02d:%02d" \
-  $((uptime_s/3600)) $(((uptime_s%3600)/60)) $((uptime_s%60))
-echo "Uptime - $uptime_hms"
-
 days=$((uptime_s/86400))
 hours=$(((uptime_s%86400)/3600))
 mins=$(((uptime_s%3600)/60))
-echo "Uptime - ${days}d ${hours}h ${mins}m"
-
+echo "Uptime: ${days}d ${hours}h ${mins}m"
+get_user_information
 get_os_information
 get_time_information
 get_cpu_information
